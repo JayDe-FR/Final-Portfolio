639ad56b8079d1c3770287735db4cb325b814869
Jayson Delion <delionjayson@gmail.com>
2021-07-26 13:26:06 +0200

    CSS complete

--- script/scriptRGB.js
+++ script/scriptRGB.js
@@ -1,22 +1,21 @@
 "use strict";
 
 const title = document.querySelector("h1");
-const twitterLink = document.querySelector("p");
+const twitterLink = document.querySelector(".twitter");
+const icons = document.querySelectorAll(".fas");
 const btn = document.querySelector(".btn");
 const displayArea = document.querySelector("#display");
 
-// .Color Randomizer ==> RGB for now
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () =>
-  `rgb(${randomInt(200, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-console.log(randomColor());
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
 
-// .Event
 btn.addEventListener("click", function () {
   const newColor = randomColor();
   displayArea.style.backgroundColor = newColor;
   displayArea.textContent = newColor;
   title.style.color = newColor;
   twitterLink.style.color = newColor;
+  icons.forEach((icon) => (icon.style.color = newColor));
 });
