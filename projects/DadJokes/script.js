a1d4df72ded319a5205ae44bb6e8ee9426cfe6dd
Jayson Delion <delionjayson@gmail.com>
2021-08-14 22:40:19 +0200

    dad jokes API quick fixes

--- projects/DadJokes/script.js
+++ projects/DadJokes/script.js
@@ -5,10 +5,6 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 jokeBtn.addEventListener("click", generateJoke);
 
-const renderJoke = function() {
-  const markup = ``;
-}
-
 async function generateJoke() {
   const config = {
     headers: { Accept: "application/json" },
@@ -16,7 +12,6 @@ async function generateJoke() {
   const response = await fetch(" https://icanhazdadjoke.com", config);
   const data = await response.json();
   jokeEl.innerHTML = data.joke;
-  renderJoke();
 }
 
 generateJoke();
